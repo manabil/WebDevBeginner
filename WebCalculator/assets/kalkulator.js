034de9016d6d8a0134c24329437a1fb965f9fa9f
manabil <mammarnabil@students.unnes.ac.id>
2022-10-04 09:38:12 +0700

    Add history feature`

--- WebCalculator/assets/kalkulator.js
+++ WebCalculator/assets/kalkulator.js
@@ -2,29 +2,29 @@
 console.log("Selamat Anda berhasil menggunakan JavaScript pada Website");
 
 const calculator = {
-    displayNumber: '0',
-    operator: null,
-    firstNumber: null,
-    isWaitForSecondNumber: false,
+  displayNumber: '0',
+  operator: null,
+  firstNumber: null,
+  isWaitForSecondNumber: false,
 };
 
 function updateDisplay() {
-    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+  document.querySelector('#displayNumber').innerText = calculator.displayNumber;
 }
 
 function clearCalculator() {
-    calculator.displayNumber = '0';
-    calculator.operator = null;
-    calculator.firstNumber = null;
-    calculator.isWaitForSecondNumber = false;
+  calculator.displayNumber = '0';
+  calculator.operator = null;
+  calculator.firstNumber = null;
+  calculator.isWaitForSecondNumber = false;
 }
 
 function inputDigit(digit) {
-    if (calculator.displayNumber === '0') {
-        calculator.displayNumber = digit;
-    } else {
-        calculator.displayNumber += digit;
-    }
+  if (calculator.displayNumber === '0') {
+    calculator.displayNumber = digit;
+  } else {
+    calculator.displayNumber += digit;
+  }
 }
 
 const buttons = document.querySelectorAll('.button');
@@ -35,9 +35,9 @@ for (const button of buttons) {
     const target = event.target;
 
     if (target.classList.contains('clear')) {
-        clearCalculator();
-        updateDisplay();
-        return;
+      clearCalculator();
+      updateDisplay();
+      return;
     }
     if (target.classList.contains('negative')) {
       inverseNumber();
@@ -54,47 +54,56 @@ for (const button of buttons) {
       return;
     }
     function inverseNumber() {
-        if (calculator.displayNumber === '0') {
-            return;
-        }
-        calculator.displayNumber *= -1;
+      if (calculator.displayNumber === '0') {
+        return;
+      }
+      calculator.displayNumber *= -1;
     }
  
     function handleOperator(operator) {
-        if (!calculator.isWaitForSecondNumber) {
-          calculator.operator = operator;
-          calculator.isWaitForSecondNumber = true;
-          calculator.firstNumber = calculator.displayNumber;
-       
-          // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
-          calculator.displayNumber = '0';
-        } else {
-          alert('Operator sudah ditetapkan');
-        }
+      if (!calculator.isWaitForSecondNumber) {
+        calculator.operator = operator;
+        calculator.isWaitForSecondNumber = true;
+        calculator.firstNumber = calculator.displayNumber;
+      
+        // mengatur ulang nilai display number supaya tombol selanjutnya dimulai dari angka pertama lagi
+        calculator.displayNumber = '0';
+      } else {
+        alert('Operator sudah ditetapkan');
+      }
     }
 
     function performCalculation() {
-        if (calculator.firstNumber == null || calculator.operator == null) {
-          alert('Anda belum menetapkan operator');
-          return;
-        }
+      if (calculator.firstNumber == null || calculator.operator == null) {
+        alert('Anda belum menetapkan operator');
+        return;
+      }
        
-        let result = 0;
-        if (calculator.operator === '+') {
-          result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
-        } else if (calculator.operator === '-') {
-          result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
-        } else if (calculator.operator === '*') {
-          result = parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
-        } else if (calculator.operator === '/') {
-          result = parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
-        } else if (calculator.operator === '(x)²') {
-          result = parseInt(calculator.firstNumber) ** 2;
-        } else if (calculator.operator === '%') {
-          result = parseInt(calculator.firstNumber) * (parseInt(calculator.displayNumber)/100);
-        }
+      let result = 0;
+      if (calculator.operator === '+') {
+        result = parseInt(calculator.firstNumber) + parseInt(calculator.displayNumber);
+      } else if (calculator.operator === '-') {
+        result = parseInt(calculator.firstNumber) - parseInt(calculator.displayNumber);
+      } else if (calculator.operator === '*') {
+        result = parseInt(calculator.firstNumber) * parseInt(calculator.displayNumber);
+      } else if (calculator.operator === '/') {
+        result = parseInt(calculator.firstNumber) / parseInt(calculator.displayNumber);
+      } else if (calculator.operator === '(x)²') {
+        result = parseInt(calculator.firstNumber) ** 2;
+      } else if (calculator.operator === '%') {
+        result = parseInt(calculator.firstNumber) * (parseInt(calculator.displayNumber)/100);
+      }
        
-        calculator.displayNumber = result;
+      const history = {
+        firstNumber: calculator.firstNumber,
+        secondNumber: calculator.displayNumber,
+        operator: calculator.operator,
+        result: result
+      }
+      
+      putHistory(history);
+      calculator.displayNumber = result;
+      renderHistory();
     }
 
     inputDigit(target.innerText);
